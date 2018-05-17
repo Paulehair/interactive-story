5459981351323a727a2c23bc53b19473909ae048
Marion <marion_ott@me.com>
2018-05-17 16:05:58 +0200

    data.json quick fix

--- js/main.js
+++ js/main.js
@@ -6,27 +6,26 @@ function ajaxRequest() {
   query.onreadystatechange = function () {
     if (query.readyState != 4 || query.status != 200) return;
     data = JSON.parse(query.responseText);
-    console.log(data.steps[0].name);
   };
   query.send();
 }
 
 ajaxRequest();
 
-const domElem = {
-  txtDisplay : document.querySelector('.Screen_dialog_txt'),
-}
-
-let text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-
-domElem.txtDisplay.innerHTML = text;
-
-let index = 0;
-domElem.txtDisplay.innerHTML = '';
-
-let interval = setInterval(function() {
-  domElem.txtDisplay.innerHTML += text[index++];
-  if (index === text.length) {
-    clearInterval(interval);
-  }
-}, 20);
+// const domElem = {
+//   txtDisplay : document.querySelector('.Screen_dialog_txt'),
+// }
+//
+// let text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+//
+// domElem.txtDisplay.innerHTML = text;
+//
+// let index = 0;
+// domElem.txtDisplay.innerHTML = '';
+//
+// let interval = setInterval(function() {
+//   domElem.txtDisplay.innerHTML += text[index++];
+//   if (index === text.length) {
+//     clearInterval(interval);
+//   }
+// }, 20);
