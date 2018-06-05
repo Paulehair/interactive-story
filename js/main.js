fbdcb10b78ef2d7d27f1ccb43f165c4a816e40b9
Paule Herman <hermanpaule@gmail.com>
2018-06-05 22:49:53 +0200

    restart game function

--- js/main.js
+++ js/main.js
@@ -13,10 +13,13 @@ fetch('./js/data.json')
     console.log('Fetch Error', err);
   });
 
+var map = document.querySelector('.Container__Map');
 var textArea = document.querySelector('.Container__Map__Instructions__Txt');
 var mapArea = document.querySelector('.Container__Map__sceneImg');
 var firstChoice = document.querySelector('#firstChoice');
 var secondChoice = document.querySelector('#secondChoice');
+var screen = document.querySelector('#game-start');
+var end = document.querySelector('#game-end');
 var index = 0;
 
 function typing(text) {
@@ -39,12 +42,12 @@ function displayItems(param1, param2, param3, param4) {
 
 function startStory(data) {
   var start = document.querySelector('#start');
-  var screen = document.querySelector('#game-start')
   start.addEventListener('click', function() {
     screen.classList.remove('is-open');
     displayItems(data.steps[index].description, data.steps[index].option1.description, data.steps[index].option2.description, data.steps[index].img_src);
     firstChoice.addEventListener('click', function() {
       index = data.steps[index].option1.next;
+      console.log(index);
       if (index <= 9) {
         displayItems(data.steps[index].description, data.steps[index].option1.description, data.steps[index].option2.description, data.steps[index].img_src);
       } else {
@@ -52,10 +55,12 @@ function startStory(data) {
         // firstChoice.innerHTML = data.steps[index].message;
         // secondChoice.remove();
         displayEnd(data.steps[index].description);
+        restartGame();
       }
     });
     secondChoice.addEventListener('click', function() {
       index = data.steps[index].option2.next;
+      console.log(index);
       if (index <= 9) {
         displayItems(data.steps[index].description, data.steps[index].option1.description, data.steps[index].option2.description, data.steps[index].img_src);
       } else {
@@ -63,22 +68,34 @@ function startStory(data) {
         // firstChoice.innerHTML = data.steps[index].message;
         // secondChoice.remove();
         displayEnd(data.steps[index].description);
+        restartGame();
       }
     });
   });
 }
 
 function displayEnd(msg) {
-  var map = document.querySelector('.Container__Map');
-  var end = document.querySelector('#game-end');
   var endStory = document.querySelector('#end-msg-story');
   var winLose = document.querySelector('#winLose');
   map.style.display = 'none';
   end.style.display = 'block';
+  typing(msg);
   endStory.innerHTML = msg;
-  if (index == 12) {
+  console.log(index);
+  if (index == 11) {
     winLose.innerHTML = "Vous avez gagné !"
   } else {
     winLose.innerHTML = "Vous avez perdu !"
   }
+}
+
+function restartGame() {
+  var retry = document.querySelector('#retry');
+  retry.addEventListener('click', function() {
+    map.style.display = 'block';
+    screen.classList.add('is-open');
+    end.style.display = 'none';
+    index = 0;
+    startStory();
+  })
 }
